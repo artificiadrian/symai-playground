6919f0aa1bd698bb893fb83f214c7f8edc496523
artificiadrian <36929853+artificiadrian@users.noreply.github.com>
2024-08-26 20:21:56 +0200

    fix graphs

--- graphs/create_types.py
+++ graphs/create_types.py
@@ -1,7 +1,7 @@
 import re
 from typing import Optional, cast, Literal
 
-from pydantic import Field, create_model, BaseModel
+from pydantic import Field, create_model, BaseModel, model_validator
 
 from graphs.models import (
     EnumProperty,
@@ -27,15 +27,53 @@ def _pascal_to_snake_case(pascal: str):
 
 
 class Node(StrictModel):
-    pass
+
+    def get_id(self):
+        return self.uid
+
+    def get_display_name(self):
+        return self.display_name
 
 
 class Edge(StrictModel):
-    pass
+
+    def get_id(self):
+        return self.uid
+
+    def get_display_name(self):
+        return self.display_name
+
+    @property
+    def node_a(self):
+        return self.a
+
+    @property
+    def node_b(self):
+        return self.b
 
 
 class BaseGraph(StrictModel):
-    pass
+    def get_nodes(self) -> list[Node]:
+        ...
+
+    def get_edges(self) -> list[Edge]:
+        ...
+
+    @model_validator(mode="after")
+    def validate_edge_nodes(self):
+        nodes = self.get_nodes()
+        edges = self.get_edges()
+
+        node_ids = {node.id for node in nodes}
+
+        for edge in edges:
+            if edge.a not in node_ids:
+                raise ValueError(f"Edge {edge.id} has unknown node type {edge.a}")
+
+            if edge.b not in node_ids:
+                raise ValueError(f"Edge {edge.id} has unknown node type {edge.b}")
+
+        return self
 
 
 def _create_enum_as_literal_union(prop: EnumProperty):
@@ -61,7 +99,8 @@ def _create_property_field(prop: Property):
 
 def _create_property_fields(properties: list[Property]):
     props = {p.name: _create_property_field(p) for p in properties}
-    props["id"] = (str, Field(description="Globally unique identifier of the node (camel Case)!"))
+    props["id"] = (str, Field(description="Globally unique identifier (camel Case)!"))
+    props["display_name"] = str, Field(...)
     return props
 
 
@@ -69,7 +108,6 @@ def _create_type(definition, base: type[BaseModel], **props):
     return create_model(
         definition.name,
         __base__=base,
-        __module__="custom_graph",
         **_create_property_fields(definition.properties),
         **props,
     )
@@ -96,17 +134,17 @@ def _types_to_list_fields(types: list[type[Node] | type[Edge]]):
             t in types}
 
 
-def create_graph_type(graph: GraphDefinition, module: str = "custom_graph"):
-    # TODO check if name collisions in modules are a problem!
-
+def create_graph_type(graph: GraphDefinition):
     node_types = [_create_node_type(node) for node in graph.nodes]
     edge_types = [_create_edge_type(edge) for edge in graph.edges]
+
+    node_var_names = [_pascal_to_snake_case(node.__name__) for node in node_types]
+    edge_var_names = [_pascal_to_snake_case(edge.__name__) for edge in edge_types]
 
     fields = _types_to_list_fields(node_types + edge_types)
 
     graph_type = create_model(
         "Graph",
-        __module__=module,
         __base__=BaseGraph,
         **fields
     )
@@ -115,7 +153,19 @@ def create_graph_type(graph: GraphDefinition, module: str = "custom_graph"):
         """Creates an empty instance of the generated graph type with empty lists by default"""
         super(graph_type, self).__init__(**{k: kwargs.get(k, []) for k in fields.keys()})
 
+    def get_nodes(self):
+        lists = [getattr(self, name) for name in node_var_names]
+        return [item for sublist in lists for item in sublist]
+
+    def get_edges(self):
+        lists = [getattr(self, name) for name in edge_var_names]
+        return [item for sublist in lists for item in sublist]
+
     # set custom __init__ function s.t. calling `graph_type()` creates a graph with empty lists
     graph_type.__init__ = __init__
+
+    # add custom methods to the graph type
+    graph_type.get_nodes = get_nodes
+    graph_type.get_edges = get_edges
 
     return graph_type
--- graphs/test.py
+++ graphs/test.py
@@ -1,12 +1,18 @@
+from pathlib import Path
+
 from symai import Symbol
 
 from graphs.create_types import BaseGraph
 from graphs.expressions.create_graph import CreateGraph
 from graphs.expressions.fill_graph import FillGraph
+import networkx as nx
+from pyvis.network import Network
 
 g = CreateGraph()
 
-sym = g(Symbol("I want to build a character-relationship graph from a fiction book. Focus on characters and their relationships."))
+sym = g(Symbol(
+    "I want to build an ontology for a fiction story. Characters as nodes and relationships as edges. Add properties "
+    "to them. No other node or edge types."))
 graph_type: type[BaseGraph] = sym.value
 
 graph = graph_type()
@@ -14,11 +20,25 @@ print("Graph:", graph)
 
 print("Schema:", graph.model_json_schema())
 
-with open("examples/romeo_and_juliet.txt", "r", encoding="utf-8") as f:
-    text = Symbol(f.read()[:20000])
+paper_text = Symbol(Path("examples/romeo_and_juliet.txt").read_text(encoding="utf-8"))
 
 fg = FillGraph(graph)
 print("Filling...\n")
 
-sym = fg(text)
-print(sym.value.model_dump_json(indent=4))
+sym = fg(paper_text)
+graph = sym.value
+print(sym, graph)
+
+with open("examples/graph.json", "w") as f:
+    f.write(graph.model_dump_json(indent=4))
+
+G = nx.Graph()
+for node in graph.get_nodes():
+    G.add_node(node.uid, label=node.get_display_name())
+
+for edge in graph.get_edges():
+    G.add_edge(edge.a, edge.b, label=edge.get_display_name())
+
+network = Network()
+network.from_nx(G)
+network.save_graph("examples/graph.html")
