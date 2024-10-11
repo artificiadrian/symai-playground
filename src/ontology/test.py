a7322dbe9d84671ef2fcdc452e40b47672276726
artificiadrian <36929853+artificiadrian@users.noreply.github.com>
2024-10-11 08:57:34 +0200

    change to uv

--- src/ontology/test.py
+++ src/ontology/test.py
@@ -1,4 +1,54 @@
 import os
+from pathlib import Path
+
+import openai
+import tiktoken
+from pydantic import Field
+
+from ontology.utils import StrictModel
+
+dotenv = Path(".env").read_text()
+for line in dotenv.splitlines():
+    key, value = line.split("=")
+    os.environ[key] = value
 
 if not os.environ["OPENAI_API_KEY"]:
     raise Exception("OPENAI_API_KEY environment variable not set")
+
+text = Path("examples/wikipedia_ww2.txt").read_text(encoding="utf-8")
+
+encoding = tiktoken.get_encoding("cl100k_base")
+
+
+def chunked(text: str, chunk_size: int = 128_000):
+    tokens = encoding.encode(text)
+    for i in range(0, len(tokens), chunk_size):
+        yield encoding.decode(tokens[i: i + chunk_size])
+
+
+class Person(StrictModel):
+    id: str = Field(
+        ...,
+        description="{fname}_{lname} (snake_case!) (e.g. 'john_doe', 'jane_smith')",
+    )
+
+    first_name: str
+    last_name: str
+    description: str
+
+
+
+for chunk in chunked(text):
+    resp = openai.beta.chat.completions.parse(
+        model="gpt-4o-mini",
+        messages=[
+            {
+                "role": "system",
+                "content": "Extract ALL relevant information from the text",
+            },
+            {"role": "user", "content": chunk},
+        ],
+        response_format=AddPersons,
+    )
+
+    print(resp.choices[0].message.parsed)
--- src/ontology/utils.py
+++ src/ontology/utils.py
@@ -1,6 +1,6 @@
-from typing import Literal
+from typing import Literal, Optional
 
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, create_model
 
 
 class StrictModel(BaseModel):
@@ -9,3 +9,13 @@ class StrictModel(BaseModel):
 
 def union_literal(values: list[str]):
     return Literal[*values]  # type: ignore[reportInvalidTypeForm] # this is valid (returns Literal[values[0], values[1], ...] type)
+
+
+# TODO improve this and ensure there are only scalars, union literals and scalar lists
+def Partial(type_: type[StrictModel]) -> type[StrictModel]:
+    return create_model(
+        f"Partial{type_.__name__}",
+        __base__=type_.__base__,  # type: ignore
+        __module__=type_.__module__,
+        **{k: (Optional[v.annotation], v) for k, v in type_.model_fields.items()},  # type: ignore
+    )
